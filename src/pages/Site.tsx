41aeae2e247587122bdb079364ebe5e5b3d7495a
AkramGafurov <akramminsk@yandex.ru>
2024-09-16 23:24:22 +0300

    feat: add NavLink and Route in site for navigation

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -2,17 +2,27 @@ import styles from './Site.module.css';
 import PageOne from "./PageOne";
 import PageTwo from "./PageTwo";
 import PageThree from "./PageThree";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
+import Error404 from "./Error404";
 
 
 function Site() {
    return <div>
        <div className={styles.header}>Header</div>
        <div className={styles.body}>
-           <div className={styles.nav}>1234</div>
+           <div className={styles.nav}>
+               <NavLink to={'/pageOne'}>PageOne</NavLink>
+               <NavLink to={'/pageTwo'}>PageTwo</NavLink>
+               <NavLink to={'/pageThree'}>PageThree</NavLink>
+               </div>
            <div className={styles.content}>
-               <PageOne/>
-               <PageTwo/>
-               <PageThree/>
+               <Routes>
+                   <Route path={"/*"} element={<Error404/>} />
+                   <Route path={"/"} element={<Navigate to={'/pageOne'}/>} />
+                   <Route path={"/pageOne"} element={<PageOne/>} />
+                   <Route path={"/pageTwo"} element={<PageTwo/>} />
+                   <Route path={"/pageThree"} element={<PageThree/>} />
+               </Routes>
            </div>
        </div>
    </div>
